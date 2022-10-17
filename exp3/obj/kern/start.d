175417691279a7a7f0ec26b0fcf4a6a4b18b57a5
sekiro <1356464784@qq.com>
2022-10-18 00:55:41 +0800

    copy from lab

---
+++ exp3/obj/kern/start.d
@@ -0,0 +1,3 @@
+obj/kern/start.o: kern/start.c /usr/include/stdc-predef.h inc/type.h \
+ inc/protect.h inc/type.h inc/string.h inc/cmatrix.h inc/terminal.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
