1a8c212c993b99279fc0e9df44738e32363489f2
sekiro <1356464784@qq.com>
2022-12-06 16:36:34 +0800

    给新进程分配页表

--- exp2/inc/kern/kmalloc.h
+++
@@ -1,8 +0,0 @@
-#ifndef MINIOS_KERN_KMALLOC_H
-#define MINIOS_KERN_KMALLOC_H
-
-phyaddr_t	phy_malloc_4k(void);
-phyaddr_t	phy_malloc_user(u32 size);
-void	 	user_alloc_pte(phyaddr_t cr3, phyaddr_t start_va, u32 size);
-
-#endif /* MINIOS_KERN_KMALLOC_H */
--- exp2/inc/kern/pmap.h
+++
@@ -1,8 +0,0 @@
-#ifndef MINIOS_KERN_PMAP_H
-#define MINIOS_KERN_PMAP_H
-
-#include <type.h>
-
-void		map_kern(phyaddr_t cr3);
-
-#endif
